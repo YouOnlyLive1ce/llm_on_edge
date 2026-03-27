53ec005fb6b8e09ac5d32586a16fa5097c5a4229
YouOnlyLive1ce <123145672+YouOnlyLive1ce@users.noreply.github.com>
2026-03-27 23:32:10 +0300

    smollm2 experiments res

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 import subprocess
 import requests
 import json
+# from memory_profiler import memory_usage
 
 class BenchLLM:
     def __init__(self,serv_path,ppl_path,model_path):
@@ -14,41 +15,52 @@ class BenchLLM:
                       'accuracy':[],
                       'ppl':[],
                       'flips':[],
+                      'prompt_per_second':[],
+                      'predicted_per_second':[]
                       }
     def start_server(self):
         # pipe have response, statistics
         self.pipe=subprocess.Popen([self.executable_server_path, '-m', self.model_path, '--port','8081'], stdout=subprocess.PIPE)
+        # self.memory_usage=memory_usage(self.pipe, interval=60,multiprocess=True)
+    
     def stop_server(self):
         self.pipe.stdout.close()
         self.pipe.terminate()
+        # print(self.memory_usage)
+        
     def bench(self):
         headers = {"Content-Type": "application/json"}
         amount=2
-        for i in range(amount):
+        # hotpotqa is a fact knowledge checking dataset. if any of words from response is in target, count as correct
+        for i in range(amount+1,201):
             print(i)
-            row=self.dataset['train'][i]
+            row=self.dataset['validation'][i]
             # request have question and context titles
-            data={ "prompt": row['question']+"".join(row['context']['title']),"n_predict": 256}
+            data={ "prompt": "Topic: "+" ".join(row['context']['title'])+" Question: "+row['question']+" AI assistant answer: ","n_predict": 32}
             response=requests.post('http://localhost:8081/completion',headers=headers,json=data).json()
             # save stats
             self.results['token/sec'].append(response['timings']['predicted_per_second'])
+            self.results['prompt_per_second'].append(response['timings']['prompt_per_second'])
+            self.results['predicted_per_second'].append(response['timings']['predicted_per_second'])
             self.results['accuracy'].append(any(word in response['content'].split() for word in row['answer'].split()))
+            print(response)
         self.stop_server()
         
         # traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
-        ppl_pipe=subprocess.Popen(
-            [self.executable_ppl_path, '-m', self.model_path, '--chunks', str(amount), '-f', './wikitext-2-raw/wiki.train.raw'], 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        stdout,stderr=ppl_pipe.communicate()
-        ppl_res=stdout[stdout.find('minutes'):stdout.rfind('estimate')]
-        for num in ppl_res.split(',')[:-1]:
-            num=num[3:]
-            self.results['ppl'].append(num)
+        # ppl_pipe=subprocess.Popen(
+        #     [self.executable_ppl_path, '-m', self.model_path, '--chunks', str(amount), '-f', '/mnt/d/Inno/Thesis/llama.cpp/wikitext-2-raw/wiki.test.raw'], 
+        #     stdout=subprocess.PIPE, 
+        #     stderr=subprocess.PIPE,
+        #     text=True
+        # )
+        # stdout,stderr=ppl_pipe.communicate()
+        # ppl_res=stdout[stdout.find('minutes'):stdout.rfind('estimate')]
+        # for num in ppl_res.split(',')[:-1]:
+        #     num=num[3:]
+        #     self.results['ppl'].append(num)
+    
     def save_to_file(self,filename):
-        with open(f'./benchmark/benchmark_results/{filename}.json','w') as json_file:
+        with open(f'./{filename}.json','w') as json_file:
             json.dump(self.results,json_file,indent=2)
     
     def compare_results(self, path_to_jsons):
@@ -56,7 +68,6 @@ class BenchLLM:
         import pandas as pd
         import numpy as np
 
-        path_to_jsons = '.'
         # last df has highest bitwidth and hence used to compare with
         json_files = [pos_json for pos_json in os.listdir(path_to_jsons) if pos_json.endswith('.json')]
         dfs=[]
@@ -77,6 +88,7 @@ class BenchLLM:
         for i in range(len(dfs)):
             if i==best_model_idx:
                 continue
+            # big original model has correct, but now it is incorrect
             dfs[i]['flips']=(dfs[i]['accuracy']==0.0) & (dfs[best_model_idx]['accuracy']==1.0)
 
         pd.set_option('display.float_format', '{:0.20f}'.format)
@@ -89,6 +101,7 @@ class BenchLLM:
             )
         result_df=pd.DataFrame(result_df)
         return result_df
+    
     def calc_corr(self,result_df):
         tmp_df=result_df.copy(deep=True)
         tmp_df['token/sec']=tmp_df['token/sec'].apply(lambda x: float(x.split('+-')[0]))
